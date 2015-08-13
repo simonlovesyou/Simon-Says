ec02ca7d260df9d4d8bc98847f19032835702a06
Simon Johansson <SimonLJohansson92@gmail.com>
2015-08-13 22:29:20 +0200

    structural changes

--- modules/index.js
+++ modules/index.js
@@ -4,27 +4,57 @@ import dirsortjs from './logic'
 import path from 'path';
 import http from 'http';
 import Promise from 'bluebird';
+import fs from 'fs';
 
-let app = express();
+
+let stat = Promise.promisify(fs.stat),
+    app = express();
 
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'jade');
 app.set('view options', {
   layout: false
 });
-app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.static(path.join(__dirname, '../client/')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+require('./routes')(app);
+
 http.createServer(app).listen('8151', () => {
   console.log('Express server listening on port ' + 8151);
-  //console.log('Visit @ ' + config.DOMAIN);
 });
 
-console.log(dirsortjs.start);
-
 let config;
+
+
+app.get('/', function (req, res) {
+  console.log('config:'+config);
+  res.render('content', {layout: 'layout', folders: config});
+});
+
+app.get('/overview', function (req, res) {
+
+  res.render('overview', {layout: 'layout', lol: 'hejdå'});
+});
+
+app.get('/folders', function (req, res) {
+
+  res.render('folders', {layout: 'layout', folders: config});
+});
+
+app.get('/add/folder', function (req, res) {
+
+  res.render('add_folder', {layout: 'layout'});
+});
+
+app.get('/client/index.js', function (req, res) {
+
+  res.sendFile(path.join(__dirname, '../client/index.js'));
+});
+
 
 
 dirsortjs.start(function(result, err) {
