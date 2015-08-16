acad96a3d28fe590aabf1ab28fda7cc3f7ead88d
Simon Johansson <SimonLJohansson92@gmail.com>
2015-08-16 22:44:47 +0200

    👍 add ajax request to server

--- client/index.js
+++ client/index.js
@@ -3,17 +3,68 @@ var index = {
   'taskList': '.row > .col-md-6:nth-child(2) > ul'
 }
 
-
-
 $(document).ready(function() {
   $('#folderSave').on('click', function() {
 
-    $.post('/api/folders/add/', {"folder": $('#folderName').val()})
-    .done(function() {
-      console.log("Valid directory!");
-    })
-    .fail(function() {
-      console.log("Not valid directory!");
+    $.ajax({
+      url: '/api/folders/add',
+      type: 'post',
+      data: {"folder": $('#folderName').val()},
+      statusCode: {
+        200: function(res) {
+          console.log("Valid directory!");
+        },
+        201: function(res) {
+          console.log("Valid directory!");
+        },
+        400: function(res) {
+          console.log("Not a valid directory!");
+        },
+        420: function(res) {
+          console.log("Not a valid directory");
+        }
+      }
     });
   });
+
+  $('#taskSave').on('click', function() {
+    console.log("Jag trycks");
+    console.log($(index.folderList).children().find('.active-folder'));
+    var folderName = $(index.folderList).children().find('.active-folder').find('h3').html();
+    var folderPath = $(index.folderList).children().find('.active-folder').find('p').find('i').html();
+    console.log("Foldername: %s, folderpath: %s", folderName, folderPath);
+    $.ajax({
+      url: '/api/tasks/add',
+      type: 'post',
+      data: {
+        "folderName": folderName,
+        "folderPath": folderPath,
+        "taskName": $('#taskName').val(),
+        "taskDescription": $('#taskDescription').val()
+      },
+      statusCode: {
+        200: function(res) {
+          console.log("Valid directory!");
+        },
+        201: function(res) {
+          console.log("Valid directory!");
+        },
+        400: function(res) {
+          console.log("Not a valid directory!");
+        },
+        420: function(res) {
+          console.log("Not a valid directory");
+        }
+      }
+    });
+  });
+
+  $(index.folderList).children().each(function() {
+    $(this).on('click', function() {
+      console.log($(this).siblings());
+      $(this).siblings().find('div').removeClass('active-folder');
+      $(this).find('div').addClass('active-folder');
+    });
+  })
 });
+
