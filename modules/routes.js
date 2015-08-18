5e5bea87867738db9f9e7f01aef5eeb80994ada7
Simon Johansson <SimonLJohansson92@gmail.com>
2015-08-18 21:43:54 +0200

    👀 add routing to api/tasks

--- modules/routes.js
+++ modules/routes.js
@@ -8,10 +8,9 @@
 module.exports = function(app) {
 
   app.use('/api/folders', require('./api/folders'));
+  app.use('/api/tasks/', require('./api/tasks'));
 
   // All undefined asset or api routes should return a 404
   /*app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);*/
 }
-
-
