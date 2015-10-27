df24a24214fd5d1ed77a9bac019e7f49e1ea4699
Simon Johansson <SimonLJohansson92@gmail.com>
2015-10-28 00:05:02 +0900

    move files

--- modules/background/index.js
+++ modules/background/index.js
@@ -1,22 +1,12 @@
-import express from 'express';
-import bodyParser from 'body-parser'
 import dirsortjs from './logic'
 import path from 'path';
 import http from 'http';
 import Promise from 'bluebird';
-import configHelper from './api/configHelper.js'
+import configHelper from './api/configHelper.js';
+import app from './app.js';
+import background from './background.js';
 
-let app = express();
-
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'jade');
-app.set('view options', {
-  layout: false
-});
-
-app.use(express.static(path.join(__dirname, '../client/')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+background();
 
 http.createServer(app).listen('8151', () => {
   console.log('Express server listening on port ' + 8151);
@@ -25,14 +15,12 @@ http.createServer(app).listen('8151', () => {
 require('./routes')(app);
 
 app.get('/', function (req, res) {
-
   configHelper.get().then(config => {
     res.render('content', {layout: 'layout', folders: config});
   });
 });
 
 app.get('/overview', function (req, res) {
-
   res.render('overview', {layout: 'layout'});
 });
 
@@ -59,17 +47,20 @@ app.get('/client/main.css', function (req, res) {
 
 
 
-dirsortjs.start(function(result, err) {
+/*dirsortjs.start(function(result, err) {
 
   if(err) {
     throw Error(err);
   }
-});
+});*/
 
 process.on('uncaughtException', function (er) {
   console.error(er.stack)
   process.exit(1)
 });
+
+
+
 
 // Expose app
 exports = module.exports = app;
