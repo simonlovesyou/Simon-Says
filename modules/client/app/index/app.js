d126f934c3ab0c5812b2ea36fe0f5429c3d84e2e
Simon Johansson <SimonLJohansson92@gmail.com>
2015-11-01 16:42:20 +0900

    move files to use an angular project folder structure

--- modules/client/app/index/app.js
+++ modules/client/app/index/app.js
@@ -1,23 +1,23 @@
 // Declare app level module which depends on filters, and services
-import angular from 'angular';  
+import angular from 'angular';
 
 var simonSays = angular.module('SimonSays', []);
 
+console.log(simonSays);
 
 /*simonSays.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider
     .when('/', {
-      templateUrl: '../client/index/index.html'
+      templateUrl: 'index.html'
       //,
       //controller: IndexCtrl
     })
     .when('/folders', {
-      templateUrl: '../client/folders/folders.html',
+      templateUrl: '../folders/folders.html',
       controller: 'FoldersCtrl'
     })
     .otherwise({
       redirectTo: '/'
     });
     $locationProvider.html5Mode(true);
-  }]);
-*/
+}]);*/
