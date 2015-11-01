db6fa530542bc81ab33b470081e3a101efce39da
Simon Johansson <SimonLJohansson92@gmail.com>
2015-11-01 22:21:06 +0900

    add folder.controller to client

--- modules/client/app/folders/folders.controller.js
+++ modules/client/app/folders/folders.controller.js
@@ -1,8 +1,27 @@
 'use strict';
-var appModule = angular.module('SimonSays');
-appModule.controller('FolderCtrl', function ($scope, achievementsService, userData) {
+import angular from 'angular';
+import $ from 'jquery';
+let ipc = require('ipc');
 
+const FolderCtrl = ($scope) => {
 
+  ipc.send('folders', {query: 'get'});
+  ipc.on('folders/get', (res) => {
+    if(res.status === 200) {
+      $scope.folders = res.data.folders;
+      $scope.safeApply();
+    }
+  });
+  $scope.safeApply = function(fn) {
+    var phase = this.$root.$$phase;
+    if(phase === '$apply' || phase === '$digest') {
+      if(fn && (typeof(fn) === 'function')) {
+        fn();
+      }
+    } else {
+      this.$apply(fn);
+    }
+  };
+}
 
-
-});
+module.exports = FolderCtrl;
