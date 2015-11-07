ff4875911bcca6c82ca556e8faf0da86f7d79153
Simon Johansson <SimonLJohansson92@gmail.com>
2015-11-07 15:24:04 +0900

    correct adding folders to view

--- modules/client/app/folders/folders.controller.js
+++ modules/client/app/folders/folders.controller.js
@@ -5,10 +5,12 @@ let ipc = require('ipc');
 
 const FolderCtrl = ($scope) => {
 
+  //Get all folders when page loads
   ipc.send('folders', {query: 'get'});
   ipc.on('folders/get', (res) => {
+    console.log(res);
     if(res.status === 200) {
-      $scope.folders = res.data.folders;
+      $scope.folders = res.data;
       $scope.safeApply();
     } else if(res.status === 404) {
       console.log('Could not find the folders')
