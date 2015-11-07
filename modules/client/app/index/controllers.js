023f97a5f2e70c39188c9569a91050df5f067088
Simon Johansson <SimonLJohansson92@gmail.com>
2015-11-07 15:02:16 +0900

    remove block comment with code

--- modules/client/app/index/controllers.js
+++ modules/client/app/index/controllers.js
@@ -1,20 +1,2 @@
 'use strict';
-
-/* Controllers */
-
-/*
-
-angular.module('SimonSays', ['ngRoute'])
-.controller('AppCtrl', function ($scope, $http) {
-
-})
-.controller('IndexCtrl', function ($scope) {
-  // write Ctrl here
-
-})
-.controller('FoldersCtrl', function ($scope) {
-  // write Ctrl here
-
-});
-
-*/
+import angular from 'angular';
